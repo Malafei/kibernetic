7db15293efbfb4c86758b0dfd4f1e3f4140e9d3e
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2022-04-24 18:09:58 +0300

    commit

--- src/components/news/main/NewsPage.js
+++ src/components/news/main/NewsPage.js
@@ -108,7 +108,6 @@ const NewsPage = () => {
                                                 </div>
                                             </div>
                                         </div>
-
                                     </>
                                 );
                             })
