6311f410400fe14a652f0b647ede73fded0fb1bc
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2023-04-04 23:59:12 +0300

    підправлено гнучкість сторінок та незначні баги

--- src/components/schedule/addShedue/Action.js
+++ src/components/schedule/addShedue/Action.js
@@ -4,7 +4,7 @@ import http from "../../../http_common";
 
 export const SheduleAdd = (formData) => async(dispatch)=>{
     try{
-        const {data} = await http.post("api/Shedule/addShedule", formData, { headers: { 'Content-Type': 'multipart/form-data' }});
+        const {data} = await http.post("api/Shedule/addShedule", formData);
         dispatch({type: SHEDULE_ADD, payload: data});
         return Promise.resolve(data);
     }
